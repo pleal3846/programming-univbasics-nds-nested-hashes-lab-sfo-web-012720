65c4619ac541ff5deef1fc36b7ddbe7273021ef3
patrick leal <pjleal3846@icloud.com>
2020-01-21 11:01:26 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,7 @@ def assembled_aoh
   # Array. Ruby constants are denoted by ALL_CAPS
 end
 
-def literal_aoh
+def literal_aoh =
   [{:name=>"Don Gately", :occupation=>"Live-in Staff"},
        {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
        {:name=>"Pat Monteseian", :occupation=>"Staff"},
